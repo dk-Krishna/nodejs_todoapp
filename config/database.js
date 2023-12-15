94e90588f1a2449f79e7b1a2a33e4d671a8a19a3
krushna_dike_patil <149665601+dk-Krishna@users.noreply.github.com>
2023-12-15 12:06:57 +0530

    engine update

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URI, { dbName: "Backend-Todo" }).then(()=> {
-    console.log("MongoDB connected successfully");
+mongoose.connect(process.env.MONGO_URI, { dbName: "Backend-Todo" }).then((c)=> {
+    console.log(`MongoDB connected successfully with ${c.connection.host}`);
 }).catch((err)=> {
     console.log(err);
 });
